82fa6c6f88369d68bd732fecee765a811cd185f8
Felipe <felipe.baleche2@gmail.com>
2025-10-20 13:32:58 -0300

    cleanup: remove client-contract import test

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,16 +1,4 @@
-import { contract } from "contract";
-import { initClient } from "@ts-rest/core";
-
-const client = initClient(contract, {
-    baseUrl: "http://localhost:8000",
-    baseHeaders: {},
-});
-
 function App() {
-    let result = client.Authentication.login({
-        body: { email: "felie", password: "fajdlks;" },
-    });
-    console.log(result);
     return (
         <>
             <h1>Client side</h1>
