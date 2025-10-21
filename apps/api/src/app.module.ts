c0e1b6b8296748011d60a32e0bb7f14b8b8c2943
Felipe <felipe.baleche2@gmail.com>
2025-10-21 12:47:32 -0300

    feat: add db integration with TypeORM

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import validate from './config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ validate })],
+  imports: [
+    ConfigModule.forRoot({ validate }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true, //IMPORTANT: set to false in production environments
+      }),
+    }),
+  ],
 })
 export class AppModule {}
