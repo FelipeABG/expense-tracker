5cd160a6b1e773e0080d449b871dca533f72695b
Felipe Augusto <felipe.baleche2@gmail.com>
2025-10-21 21:35:20 -0300

    feat: add node_env, dev_db_url environment variables and add different dbs according do node_env

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,21 +1,44 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import validate from './config/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import validate from "./config/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ validate }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get('DB_URL'),
-        autoLoadEntities: true,
-        synchronize: true, //IMPORTANT: set to false in production environments
-      }),
-    }),
-  ],
+    imports: [
+        ConfigModule.forRoot({ validate }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                const ENV = configService.get("NODE_ENV");
+                if (ENV === "production") {
+                    return {
+                        type: "postgres",
+                        url: configService.get("DB_URL"),
+                        autoLoadEntities: true,
+                        synchronize: false,
+                    };
+                } else if (ENV === "test") {
+                    return {
+                        type: "sqlite",
+                        database: ":memory:",
+                        synchronize: true,
+                        autoLoadEntities: true,
+                        dropSchema: true,
+                    };
+                } else {
+                    // ENV_NODE=development
+                    return {
+                        type: "postgres",
+                        url: configService.get("DEV_DB_URL"),
+                        autoLoadEntities: true,
+                        synchronize: true,
+                    };
+                }
+            },
+        }),
+        UserModule,
+    ],
 })
 export class AppModule {}
--- apps/api/src/config/config.ts
+++ apps/api/src/config/config.ts
@@ -1,29 +1,48 @@
-import { plainToInstance } from 'class-transformer';
-import { IsNumber, IsString, Max, Min, validateSync } from 'class-validator';
+import { plainToInstance } from "class-transformer";
+import {
+    IsEnum,
+    IsNumber,
+    IsString,
+    Max,
+    Min,
+    validateSync,
+} from "class-validator";
+
+enum Environment {
+    Production = "production",
+    Development = "development",
+    Test = "test",
+}
 
 class EnviromentVariables {
-  @IsNumber()
-  @Min(0)
-  @Max(65535)
-  PORT: number;
+    @IsNumber()
+    @Min(0)
+    @Max(65535)
+    PORT: number;
 
-  @IsString()
-  DB_URL: string;
+    @IsString()
+    DB_URL: string;
 
-  @IsString()
-  SECRET_KEY: string;
+    @IsString()
+    DEV_DB_URL: string;
+
+    @IsString()
+    SECRET_KEY: string;
+
+    @IsEnum(Environment)
+    NODE_ENV: string;
 }
 
 export default function validate(config: Record<string, unknown>) {
-  const validatedConfig = plainToInstance(EnviromentVariables, config, {
-    enableImplicitConversion: true,
-  });
+    const validatedConfig = plainToInstance(EnviromentVariables, config, {
+        enableImplicitConversion: true,
+    });
 
-  const errors = validateSync(validatedConfig);
+    const errors = validateSync(validatedConfig);
 
-  if (errors.length > 0) {
-    throw Error(errors.toString());
-  }
+    if (errors.length > 0) {
+        throw Error(errors.toString());
+    }
 
-  return validatedConfig;
+    return validatedConfig;
 }
