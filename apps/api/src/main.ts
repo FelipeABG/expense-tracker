67f78d3de2aad2afa79e20ab26daf2197c6e1683
Felipe <felipe.baleche2@gmail.com>
2025-10-23 12:50:37 -0300

    styles: formatting changes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,30 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { contract } from 'contract';
-import { generateOpenApi } from '@ts-rest/open-api';
-import { SwaggerModule } from '@nestjs/swagger';
-import { description } from './utils/description';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { contract } from "contract";
+import { generateOpenApi } from "@ts-rest/open-api";
+import { SwaggerModule } from "@nestjs/swagger";
+import { description } from "./utils/description";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const configService = app.get(ConfigService);
-  const PORT = configService.get('PORT');
+    const app = await NestFactory.create(AppModule, { cors: true });
+    const configService = app.get(ConfigService);
+    const PORT = configService.get("PORT");
 
-  const document = generateOpenApi(
-    contract,
-    {
-      info: {
-        title: 'Expense Tracker API',
-        version: '1.0.0',
-        description,
-      },
-    },
-    { setOperationId: 'concatenated-path' },
-  );
+    const document = generateOpenApi(
+        contract,
+        {
+            info: {
+                title: "Expense Tracker API",
+                version: "1.0.0",
+                description,
+            },
+        },
+        { setOperationId: "concatenated-path" },
+    );
 
-  SwaggerModule.setup('docs', app, document);
-  await app.listen(PORT);
+    SwaggerModule.setup("docs", app, document);
+    await app.listen(PORT);
 }
 
 bootstrap();
