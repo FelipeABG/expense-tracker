1043ed80a33f5863242a2be31145d79c4b3bc60f
Felipe Augusto <felipe.baleche2@gmail.com>
2025-10-23 19:08:25 -0300

    refactor: simplified auth controller (nest runtime handles the errors)

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,13 +1,8 @@
-import {
-    ConflictException,
-    Controller,
-    NotFoundException,
-    UnauthorizedException,
-} from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { tsRestHandler, TsRestHandler } from "@ts-rest/nest";
 import { Public } from "./auth.decorator";
-import { contract, INTERNAL_SERVER_ERROR } from "contract";
+import { contract } from "contract";
 
 @Controller()
 @Public()
@@ -18,44 +13,23 @@ export class AuthController {
     async handler() {
         return tsRestHandler(contract.Authentication, {
             signup: async ({ body }) => {
-                try {
-                    await this.authService.signup(body.email, body.password);
+                await this.authService.signup(body.email, body.password);
 
-                    return {
-                        status: 201,
-                        body: { message: "User registered successfully" },
-                    };
-                } catch (err) {
-                    if (err instanceof ConflictException) {
-                        return {
-                            status: 409,
-                            body: { message: err.message },
-                        };
-                    }
-                    return INTERNAL_SERVER_ERROR;
-                }
+                return {
+                    status: 201,
+                    body: { message: "User registered successfully" },
+                };
             },
 
             login: async ({ body }) => {
-                try {
-                    const result = await this.authService.login(
-                        body.email,
-                        body.password,
-                    );
-                    return {
-                        status: 200,
-                        body: result,
-                    };
-                } catch (err) {
-                    if (err instanceof NotFoundException) {
-                        return { status: 404, body: { message: err.message } };
-                    }
-                    if (err instanceof UnauthorizedException) {
-                        return { status: 401, body: { message: err.message } };
-                    }
-
-                    return INTERNAL_SERVER_ERROR;
-                }
+                const result = await this.authService.login(
+                    body.email,
+                    body.password,
+                );
+                return {
+                    status: 200,
+                    body: result,
+                };
             },
         });
     }
