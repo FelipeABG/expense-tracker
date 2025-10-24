46a9ceca703ca223bb28d497fe7357406a922d92
Felipe <felipe.baleche2@gmail.com>
2025-10-24 16:19:55 -0300

    test: add user controller tests

---
+++ apps/api/test/user.e2e-spec.ts
@@ -0,0 +1,220 @@
+import { Test } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import request from "supertest";
+import { App } from "supertest/types";
+import { AppModule } from "../src/app.module";
+import { UserService } from "../src/user/user.service";
+import { Role } from "../src/role/role.enum";
+import bcrypt from "bcryptjs";
+
+describe("UserController (e2e)", () => {
+    let app: INestApplication<App>;
+    let userToken: string;
+    let adminToken: string;
+    const user = {
+        email: `test-user${Date.now()}@gmail.com`,
+        password: "Strongassoword123!",
+    };
+    const admin = {
+        email: `test-admin${Date.now()}@gmail.com`,
+        password: "Strongassoword123!",
+    };
+    const path = "/users";
+
+    beforeAll(async () => {
+        const modRef = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = modRef.createNestApplication();
+        await app.init();
+
+        //Creating and getting user token
+        await request(app.getHttpServer()).post("/auth/signup").send(user);
+        userToken = await request(app.getHttpServer())
+            .post("/auth/login")
+            .send(user)
+            .then((response) => response.body.token);
+
+        //creating and getting admin token
+        await modRef.get(UserService).create({
+            email: admin.email,
+            hash: await bcrypt.hash(admin.password, 10),
+            roles: [Role.Admin],
+        });
+        adminToken = await request(app.getHttpServer())
+            .post("/auth/login")
+            .send(admin)
+            .then((response) => response.body.token);
+    });
+
+    describe("/users (GET)", () => {
+        it("Should return 200 with a list of users", async () => {
+            await request(app.getHttpServer())
+                .get(path)
+                .set("Authorization", `Bearer ${adminToken}`)
+                .expect(200)
+                .then((response) => {
+                    expect(response.body.message).toBe(
+                        "Retrieved all users successfully",
+                    );
+                    expect(response.body.users.length).toBe(2);
+                });
+        });
+
+        it("Should optionally accept 'limit' and 'offset' as query parameters", async () => {
+            await request(app.getHttpServer())
+                .get(path.concat("?limit=1&offset=0"))
+                .set("Authorization", `Bearer ${adminToken}`)
+                .expect(200)
+                .then((response) => {
+                    expect(response.body.message).toBe(
+                        "Retrieved all users successfully",
+                    );
+                    expect(response.body.users.length).toBe(1);
+                });
+        });
+
+        describe("/users/:id", () => {
+            it("Should return 200 with the specified user", async () => {
+                await request(app.getHttpServer())
+                    .get(path.concat("/1"))
+                    .set("Authorization", `Bearer ${adminToken}`)
+                    .expect(200)
+                    .then((response) => {
+                        expect(response.body.message).toBe(
+                            "Retrieved specified user successfully",
+                        );
+                        expect(response.body.user).toBeDefined();
+                    });
+            });
+
+            it("Should return 404 when the user does not exist", async () => {
+                await request(app.getHttpServer())
+                    .get(path.concat("/99999"))
+                    .set("Authorization", `Bearer ${adminToken}`)
+                    .expect(404)
+                    .then((response) => {
+                        expect(response.body.message).toBe(
+                            "Specified user does not exist",
+                        );
+                    });
+            });
+        });
+
+        describe("/users/by-email/:email", () => {
+            it("Should return 200 with the specified user", async () => {
+                await request(app.getHttpServer())
+                    .get(`/users/by-email/${user.email}`)
+                    .set("Authorization", `Bearer ${adminToken}`)
+                    .expect(200)
+                    .then((response) => {
+                        expect(response.body.message).toBe(
+                            "Retrieved specified user successfully",
+                        );
+                        expect(response.body.user).toBeDefined();
+                    });
+            });
+
+            it("Should return 404 when the user does not exist", async () => {
+                await request(app.getHttpServer())
+                    .get("/users/by-email/foo@gar.com")
+                    .set("Authorization", `Bearer ${adminToken}`)
+                    .expect(404)
+                    .then((response) => {
+                        expect(response.body.message).toBe(
+                            "Specified user does not exist",
+                        );
+                    });
+            });
+        });
+    });
+
+    describe("/users (POST)", () => {
+        it("Should return 201 when successfull", async () => {
+            await request(app.getHttpServer())
+                .post(path)
+                .send({
+                    email: `test-user${Date.now()}@gmail.com`,
+                    password: "Strongassoword123!",
+                })
+                .set("Authorization", `Bearer ${adminToken}`)
+                .expect(201)
+                .then((response) => {
+                    expect(response.body.message).toBe(
+                        "User created successfully",
+                    );
+                });
+        });
+
+        it("Should return 409 if the user already exist", async () => {
+            await request(app.getHttpServer())
+                .post(path)
+                .send(user)
+                .set("Authorization", `Bearer ${adminToken}`)
+                .expect(409)
+                .then((response) => {
+                    expect(response.body.message).toBe(
+                        "Email address is already registered",
+                    );
+                });
+        });
+    });
+
+    describe("/users (DELETE)", () => {
+        describe("/users/:id", () => {
+            it("Should return 200 when successfull", async () => {
+                await request(app.getHttpServer())
+                    .delete(path.concat("/1"))
+                    .send(user)
+                    .set("Authorization", `Bearer ${adminToken}`)
+                    .expect(200)
+                    .then((response) => {
+                        expect(response.body.message).toBe(
+                            "User deleted successfully",
+                        );
+                    });
+            });
+
+            it("Should return 404 when user does not exist", async () => {
+                await request(app.getHttpServer())
+                    .delete(path.concat("/9999"))
+                    .send(user)
+                    .set("Authorization", `Bearer ${adminToken}`)
+                    .expect(404)
+                    .then((response) => {
+                        expect(response.body.message).toBe(
+                            "Requested user does not exist",
+                        );
+                    });
+            });
+        });
+        describe("/users/by-email/:email", () => {
+            it("Should return 404 when user does not exist", async () => {
+                await request(app.getHttpServer())
+                    .delete(path.concat("/by-email/foo@bar.com"))
+                    .send(user)
+                    .set("Authorization", `Bearer ${adminToken}`)
+                    .expect(404)
+                    .then((response) => {
+                        expect(response.body.message).toBe(
+                            "Requested user does not exist",
+                        );
+                    });
+            });
+
+            it("Should return 200 when successfull", async () => {
+                await request(app.getHttpServer())
+                    .delete(path.concat(`/by-email/${admin.email}`))
+                    .send(user)
+                    .set("Authorization", `Bearer ${adminToken}`)
+                    .expect(200)
+                    .then((response) => {
+                        expect(response.body.message).toBe(
+                            "User deleted successfully",
+                        );
+                    });
+            });
+        });
+    });
+});
