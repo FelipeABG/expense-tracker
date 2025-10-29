533f11f4a2085c663e702938616b290e48a93715
Felipe <felipe.baleche2@gmail.com>
2025-10-29 12:39:18 -0300

    feat: add expenseService methods

--- apps/api/src/expense/expense.service.ts
+++ apps/api/src/expense/expense.service.ts
@@ -1,5 +1,14 @@
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import {
+    Injectable,
+    InternalServerErrorException,
+    NotFoundException,
+} from "@nestjs/common";
+import {
+    DeepPartial,
+    FindOptionsWhere,
+    QueryFailedError,
+    Repository,
+} from "typeorm";
 import { Expense } from "./expense.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 
@@ -9,4 +18,30 @@ export class ExpenseService {
         @InjectRepository(Expense)
         private readonly expenseRepository: Repository<Expense>,
     ) {}
+
+    async find(where: FindOptionsWhere<Expense>) {
+        // If no expenses are found returns empty array
+        const expenses = await this.expenseRepository.find({
+            where,
+            relations: { user: true },
+        });
+
+        return expenses.map((expense) => {
+            // Removes roles and hash from the response
+            const { hash, roles, ...rest } = expense.user;
+            return { ...expense, user: rest };
+        });
+    }
+
+    async create(expense: DeepPartial<Expense>) {
+        try {
+            return await this.expenseRepository.save(expense);
+        } catch (err) {
+            if (err instanceof QueryFailedError) {
+                throw new NotFoundException("Specified user does not exist");
+            }
+
+            throw new InternalServerErrorException();
+        }
+    }
 }
