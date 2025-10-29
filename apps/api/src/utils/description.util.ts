65ecfcac02925320df8c1f1613e2d2a9bf6204dd
Felipe Augusto <felipe.baleche2@gmail.com>
2025-10-28 21:30:42 -0300

    docs: change description

--- apps/api/src/utils/description.util.ts
+++ apps/api/src/utils/description.util.ts
@@ -1,6 +1,6 @@
 // Swagger ui API description
 export const description = `
-A comprehensive learning project demonstrating production-grade REST API fundamentals. This API showcases enterprise-level features and best practices including:
+A simple and useful personnal finance manager to help to organize your life.
 
 **Core Features:**
 - **Authentication & Authorization**: [JWT](https://www.jwt.io/introduction#what-is-json-web-token)-based authentication with [role-based access control (RBAC)](https://www.ibm.com/think/topics/rbac).
@@ -8,7 +8,7 @@ A comprehensive learning project demonstrating production-grade REST API fundame
 - **Type Safety**: Contract-first approach with [TS-REST](https://ts-rest.com) ensuring type-safe client-server communication.
 - **Configuration Management**: Environment-based configuration for different deployment stages.
 - **API Documentation**: Auto-generated interactive documentation with [Swagger](https://swagger.io)/[OpenAPI](https://www.openapis.org).
-- **Data Validation**: Robust input validation and pagination support with [Zod](https://zod.dev).
+- **Data Validation**: Robust input validation support with [Zod](https://zod.dev).
 
 **Important to note:**
 - All endpoints require authentication, except \`/auth/login\` and \`/auth/signup\`.
